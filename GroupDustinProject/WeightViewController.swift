43201cee4250763e17f394dc65dca56d323b0c38
Dustin <redhotblood1010@gmail.com>
2026-04-07 17:37:36 -0400

    yes

--- GroupDustinProject/WeightViewController.swift
+++ GroupDustinProject/WeightViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class WeightViewController: UIViewController
+{
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    let weekDays = [
+        "Monday-Weight:",
+        "Tuesday-Weight:",
+        "Wednesday-Weight:",
+        "Thursday-Weight:",
+        "Friday-Weight:",
+        "Saturday-Weight:",
+        "Sunday-Weight:"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.sectionHeaderTopPadding = 12
     }
 
 
 }
 
+extension WeightViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weekDays.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        cell.textLabel?.text = weekDays[indexPath.row]
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0: return "Week 1"
+        case 1: return "Week 2"
+        case 2: return "Week 3"
+        default: return nil
+        }
+    }
+}
