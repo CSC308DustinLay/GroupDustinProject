b14836afe2dae15c04c11244ddc17217a7da1ead
Dustin <redhotblood1010@gmail.com>
2026-04-14 18:04:56 -0400

    many changes

---
+++ GroupDustinProject/CalorieViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CalorieViewController.swift
+//  GroupDustinProject
+//
+//  Created by Heather Bishop on 4/9/26.
+//
+
+import Foundation
---
+++ GroupDustinProject/WorkoutViewController.swift
@@ -0,0 +1,8 @@
+//
+//  WorkoutViewController.swift
+//  GroupDustinProject
+//
+//  Created by Heather Bishop on 4/13/26.
+//
+
+import Foundation
